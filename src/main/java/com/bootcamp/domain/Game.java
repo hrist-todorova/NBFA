d08a841ef6ace052dfb94668d8bad56e0767e632
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2018-10-12 16:12:19 +0400

    Fixing some mistakes

--- src/main/java/com/bootcamp/domain/Game.java
+++ src/main/java/com/bootcamp/domain/Game.java
@@ -3,7 +3,9 @@ package com.bootcamp.domain;
 import com.bootcamp.domain.Coordinate;
 import com.bootcamp.domain.Player;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 public class Game {
@@ -22,7 +24,7 @@ public class Game {
         Init(w,h);
     }
 
-    void Init(int w, int h) {
+    private void Init(int w, int h) {
         map = new int[w][h];
 
         for (int x = 0; x < w; ++x)
@@ -37,63 +39,111 @@ public class Game {
         playerMap.add(new InvalidPlayer());
         playerMap.add(new Player("Player 1", 1));
         playerMap.add(new Player("Player 2", 1));
+        playerMap.get(Player1).spots.add(new Coordinate(1,1));
+        playerMap.get(Player2).spots.add(new Coordinate(8,8));
     }
 
     public void Move(Coordinate from, Coordinate to) {
-        int x = from.x;
-        int y = from.y;
-        int x1 = to.x;
-        int y1 = to.y;
-        if (map[x][y] != currentPlayer)
+        if (!CanMove(from, to))
             return;
 
-        int dx = Math.abs(x1-x);
-        int dy = Math.abs(y1-y);
+        if (map[from.x][from.y] != currentPlayer)
+            return;
+
+        int dx = Math.abs(to.x-from.x);
+        int dy = Math.abs(to.y-from.y);
 
         if(dx > 1 || dy > 1)
-            map[x][y] = EmptyCell;
+            map[from.x][from.y] = EmptyCell;
 
-        map[x1][y1] = currentPlayer;
-        OccupyNeighbours(new Coordinate(x1,y1));
-
-        if(CheckWin() != 0)
-            return;
-
+        map[to.x][to.y] = currentPlayer;
+        OccupyNeighbours(to);
         currentPlayer = OpponentTo(currentPlayer);
     }
 
-    public int CurrentPlayer()
-    {
+    private boolean CanMove(@NotNull Coordinate from, @NotNull Coordinate to) {
+        if(map[to.x][to.y] != EmptyCell)
+            return false;
+
+        return !(Math.abs(from.x-to.x) > 2 || Math.abs(from.y-to.y) > 2);
+    }
+
+    public int CurrentPlayer() {
         return currentPlayer;
     }
 
-    void OccupyNeighbours(Coordinate point) {
-        int x = point.x;
-        int y = point.y;
+    private void OccupyNeighbours(Coordinate pt) {
         for (int dx = -1; dx < 2; ++dx)
             for (int dy = -1; dy < 2; ++dy){
-                if(x+dx < 0 || x+dx > map.length || y+dy < 0 || y+dy > map[0].length)
+                if(pt.x+dx < 0 || pt.x+dx > map.length || pt.y+dy < 0 || pt.y+dy > map[0].length)
                     continue;
 
-                if(map[x+dx][y+dy] != OpponentTo(currentPlayer))
+                if(map[pt.x+dx][pt.y+dy] != OpponentTo(currentPlayer))
                     continue;
 
-                map[x+dx][y+dy] = currentPlayer;
+                map[pt.x+dx][pt.y+dy] = currentPlayer;
                 --playerMap.get(OpponentTo(currentPlayer)).points;
                 ++playerMap.get(currentPlayer).points;
             }
     }
 
-    int OpponentTo(int player) {
+    public int[][] GetMap(){
+        return map;
+    }
+
+    private int OpponentTo(int player) {
         return player == Player1 ? Player2 : Player1;
     }
 
     public int CheckWin() {
+        int sum = 0;
+
         for (int i = 1; i < playerMap.size(); ++i) {
-            if (playerMap.get(i).points == 0)
-                return OpponentTo(i);
+            if (playerMap.get(i).points == 0) {
+                FillEmptySpaces(OpponentTo(i));
+                return TheWinner();
+            }
+
+            sum += playerMap.get(i).points;
         }
 
-        return 0;
+        if(sum == 100)
+            return TheWinner();
+
+        for(int plID = 1; plID < playerMap.size(); ++plID) {
+            boolean canMove = false;
+            Player cp = playerMap.get(plID);
+            for(Coordinate spot : cp.spots)
+                for(int i = -2; i < 3; ++i)
+                    for(int j = -2; j < 3; ++j) {
+                        if (CanMove(spot, new Coordinate(spot.x + i, spot.y + j)))
+                            canMove = true;
+                    }
+                    if(!canMove) {
+                        FillEmptySpaces(OpponentTo(plID));
+                        return TheWinner();
+                    }
+        }
+
+        return -1;
+    }
+
+    private int TheWinner() {
+        if(playerMap.get(1).points == playerMap.get(2).points)
+            return 0;
+
+        if(playerMap.get(1).points > playerMap.get(2).points)
+            return 1;
+
+        return 2;
+    }
+
+    private void FillEmptySpaces(int playerId) {
+        for(int x = 0; x < map.length; ++x)
+            for(int y = 0; y < map[0].length; ++y)
+                if(map[x][y] == EmptyCell) {
+                    map[x][y] = playerId;
+                    ++playerMap.get(playerId).points;
+                }
     }
 }
--- src/main/java/com/bootcamp/domain/Player.java
+++ src/main/java/com/bootcamp/domain/Player.java
@@ -1,7 +1,11 @@
 package com.bootcamp.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     public String name;
+    public List<Coordinate> spots;
     public int points;
 
     public boolean IsValid() { return true;}
@@ -9,5 +13,6 @@ public class Player {
     public Player(String name, int points) {
         this.name = name;
         this.points = points;
+        this.spots = new ArrayList<>();
     }
 }
